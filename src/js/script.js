cc37f330572f5526992d6467cf7393434bf7548f
esqu1 <brandbest1@gmail.com>
2017-04-07 16:47:35 -0400

    meeting times

--- src/js/script.js
+++ src/js/script.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
         if(data.instructors.length != 0){ 
           var l = data.instructors.join(', ')
         }
-        return '<p><i>' + data.section_id + '</i> - ' + data.course_title + '<br><small>' + l + '</small></p>';
+        return '<p><i>' + data.section_id + '</i> - ' + data.course_title + '<br><small>' + l + '<br>' + data.meeting_days + '</small></p>';
       }
     }
   });
