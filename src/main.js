cd7eae9935efbe248486c88a76756672185fc058
Nihar Patil <nihar.patil98@gmail.com>
2017-04-07 19:18:28 -0400

    added time-scheduled function, removed debugging function line

--- src/main.js
+++ src/main.js
@@ -2,15 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const path = require('path')
-const ApiServer = require('./ApiServer.js')
 const MongoHelper = require('./MongoHelper.js')
+const ApiServer = require('./ApiServer.js')
 const ScheduledJobs = require('./ScheduledJobs.js')
- 
 var Schedule = require('node-schedule');
 
-ScheduledJobs.SendEmailsToOpenCourses();
-
-// Schedule.scheduleJob('30 * * * * *', ScheduledJobs.SendEmailsToOpenCourses);
+Schedule.scheduleJob('30 * * * * *', ScheduledJobs.SendEmailsToOpenCourses);
 
 app.use(bodyParser.urlencoded({extended: true}))
 
