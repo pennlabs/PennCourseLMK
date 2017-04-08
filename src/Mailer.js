d75ca4dcf2c5633ea394e7c8ecec579ebac771b1
esqu1 <brandbest1@gmail.com>
2017-04-08 14:35:37 -0400

    edited mailer e-mail text

--- src/Mailer.js
+++ src/Mailer.js
@@ -1,5 +1,17 @@
 var email = require("emailjs")
 
+
+var emailText = (course) => {
+	return `<h1>Penn Course LMK</h1>
+	Hello,<br><br>
+	The course you requested, ` + course + `, is now open! Hurry and sign up now!<br><br>
+	The link to Penn InTouch is <a href="https://pennintouch.apps.upenn.edu/">here</a>.<br><br>
+	If you did not get the requested course, you can sign up again.<br><br>
+	Thank you for using Penn Course LMK!<br><br>
+	Sincerely,<br>
+	<i>The Penn Course LMK team</i>`
+}
+
 var server = email.server.connect({
     user: "penncourselmk@gmail.com",
     password: process.env.LMK_PASSWORD,
@@ -10,10 +22,10 @@ var server = email.server.connect({
 
 var sendEmail = (course, email, callback) => {
 	server.send({
-	    text: course + " is openn!!!!",
 	    from: "Penn Course LMK <penncourselmk@gmail.com>",
 	    to: email,
-	    subject: course,
+	    subject: course + " is now open!",
+	    attachment: [ {data: emailText(course), alternative:true},]
 	}, function(err, message) { callback( err, message);});
 }
 
