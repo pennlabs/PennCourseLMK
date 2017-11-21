49b7ca4a0ebcdb41ca58fe3af0a3e9184d023f36
Davis Haupt <davis.haupt@gmail.com>
2017-11-20 23:56:06 -0500

    more frontend changes

--- src/js/searchbar.js
+++ src/js/searchbar.js
@@ -4,16 +4,16 @@ function getDemandIcon(demand) {
     case 1:
     case 2:
     case 3:
-      return '<i style="color: limegreen" class="fa fa-thermometer-empty" aria-hidden="true"></i>'
+      return '<i style="color: limegreen" data-toggle="tooltip" data-placement="right" title="This course is in low demand" class="fa fa-thermometer-empty" aria-hidden="true"></i>'
     case 4:
     case 5:
     case 6:
     case 7:
-      return '<i style="color: orange" class="fa fa-thermometer-half" aria-hidden="true"></i>'
+      return '<i style="color: orange" data-toggle="tooltip" data-placement="right" title="This course is in medium demand" class="fa fa-thermometer-half" aria-hidden="true"></i>'
     case 8:
     case 9:
     case 10:
-      return '<i style="color: orangered" class="fa fa-thermometer-full" aria-hidden="true"></i>'
+      return '<i style="color: orangered" data-toggle="tooltip" data-placement="right" title="This course is in high demand" class="fa fa-thermometer-full" aria-hidden="true"></i>'
   }
 }
 
