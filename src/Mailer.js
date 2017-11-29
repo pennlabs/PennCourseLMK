e8b44d5035322ee70bfaa459557ea7c3cad83bb0
Davis Haupt <davis.haupt@gmail.com>
2017-11-29 00:50:08 -0500

    changed name in email

--- src/Mailer.js
+++ src/Mailer.js
@@ -5,14 +5,14 @@ const name = 'Penn Course Alert'
 
 // ------ Helper functions ------
 const emailText = (course, signupLink, unsubscribeLink) => {
-	return `<h1>Penn Course LMK</h1>
+	return `<h1>Penn Course Alert</h1>
 	Hello,<br><br>
 	The course you requested, ` + course + `, is now open! Hurry and sign up now!<br><br>
 	The link to Penn InTouch is <a href="https://pennintouch.apps.upenn.edu/">here</a>.<br><br>
 	If you did not get the requested course, you can sign up again <a href="` + signupLink + `">here</a>.<br><br>
 	Thank you for using `+name+`!<br><br>
 	Sincerely,<br>
-	<i>The `+ name +`team</i>
+	<i>The `+ name +` team</i>
   <p>To unsubscribe from these emails, click this link: `+unsubscribeLink+`</p>
 `
 }
